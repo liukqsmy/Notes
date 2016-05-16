725f9acb17f477c98c8b4390bef4db031fe1b3b2
liukqsmy <liukqsmy@sina.com>
2016-05-16 08:58:30 +0800

    add

--- app/src/main/java/com/example/administrator/note/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/note/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.note.activity;
 
 import android.app.ListActivity;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,9 +9,12 @@ import com.example.administrator.note.R;
 
 public class MainActivity extends ListActivity {
 
+    private SimpleCursorAdapter adapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/example/administrator/note/db/NoteDB.java
+++ app/src/main/java/com/example/administrator/note/db/NoteDB.java
@@ -9,6 +9,18 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class NoteDB extends SQLiteOpenHelper{
 
+    public  static final String TABLE_NOTES = "notes";
+    public  static final String TABLE_MEDIA = "media";
+
+    public  static final String COLUMN_ID = "_id";
+    public  static final String COLUMN_NOTE_NAME = "name";
+    public  static final String COLUMN_NOTE_CONTENT = "content";
+    public  static final String COLUMN_NOTE_DATE = "date";
+
+
+    public  static final String COLUMN_MEDIA_PATH = "path";
+    public  static final String COLUMN_MEDIA_OWNER_ID = "ower_id";
+
     public NoteDB(Context context) {
         super(context, "notes", null, 1);
     }
@@ -16,7 +28,19 @@ public class NoteDB extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("create table");
+        db.execSQL("create table " + TABLE_NOTES + " (" +
+                COLUMN_ID + " integer primary key autoincrement, " +
+                COLUMN_NOTE_NAME + " text not null default \"\"," +
+                COLUMN_NOTE_CONTENT  + " text not null default \"\"," +
+                COLUMN_NOTE_DATE + " text not null default \"\"" + ")"
+        );
+
+        db.execSQL("create table " + TABLE_MEDIA + " (" +
+                COLUMN_ID + " integer primary key autoincrement, " +
+                COLUMN_NOTE_NAME + " text not null default \"\"," +
+                COLUMN_MEDIA_PATH  + " text not null default \"\"," +
+                COLUMN_MEDIA_OWNER_ID + " integer not null default 0" + ")"
+        );
     }
 
     @Override
